c80ef3c42bf429faabcbdbd7dff1e71446da7a78
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-04-24 16:01:16 -0700

    Adding better paper display

--- js/papers.js
+++ js/papers.js
@@ -21,6 +21,8 @@ export const notebook = new Runtime().module(define, (name) => {
     "viewof myPapers",
     "onScoreUpdate",
     "maxRSelector",
+    "whatToAttend",
+    "papersListElement"
   ];
 
   if (cells.includes(name)) return renderCellIfelementExists(name);
@@ -30,16 +32,53 @@ async function onResize() {
   console.log("resize", document.getElementById("mainSection").offsetWidth);
   notebook.redefine(
     "width",
-    document.getElementById("mainSection").offsetWidth
+    document.getElementById("mainSection").offsetWidth - 50
   );
   notebook.redefine("height", window.innerHeight * 0.2);
   const maxR = await notebook.value("maxRSelector");
   maxR.value = 50;
   maxR.dispatchEvent(new Event("input"), { bubbles: true });
-  notebook.redefine("myPapersHeight", window.innerHeight * 0.9);
+  // notebook.redefine("myPapersHeight", window.innerHeight * 0.8);
 }
 window.addEventListener("resize", onResize);
 onResize();
 
 loadHTMLInto("navigation.html", document.getElementById("navigation"));
 loadHTMLInto("footer.html", document.getElementById("footer"));
+
+const authorsSelector = await notebook.value("authorsSelector");
+const affiliationsSelector = await notebook.value("affiliationsSelector");
+const tracksSelector = await notebook.value("tracksSelector");
+const spanAuthorsSelected = document.getElementById("spanAuthorsSelected");
+const spanAffiliationsSelected = document.getElementById(
+  "spanAffiliationsSelected"
+);
+const spanTracksSelected = document.getElementById("spanTracksSelected");
+
+function onUpdateFilters() {
+  spanTracksSelected.innerText = `(${tracksSelector.value.length})`;
+  spanAuthorsSelected.innerText = authorsSelector.value.length
+    ? `(${authorsSelector.value.length})`
+    : "";
+  spanAffiliationsSelected.innerText = affiliationsSelector.value.length
+    ? `(${affiliationsSelector.value.length})`
+    : "";
+}
+
+authorsSelector.addEventListener("input", onUpdateFilters);
+affiliationsSelector.addEventListener("input", onUpdateFilters);
+tracksSelector.addEventListener("input", onUpdateFilters);
+
+const paperSelectionCount = document.getElementById("paperSelectionCount");
+const myPapers = await notebook.value("myPapers");
+
+function onMyPapersChange() {
+  console.log("onMyPapersChange", myPapers.value);
+  const selected = myPapers.value;
+  paperSelectionCount.innerText = `(${selected.length})`;
+}
+myPapers.addEventListener("input", onMyPapersChange);
+
+onMyPapersChange();
+
+onUpdateFilters();
